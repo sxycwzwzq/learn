b55a0b48a43451bbf21d3fee4a8357d74f18eaaf
Ben Wang <ben@adrise.com>
2016-09-25 22:48:54 -0700

    clear code

--- chatServer.js
+++ chatServer.js
@@ -9,36 +9,29 @@ const net = require('net');
 
 const clients = [];
 
-// Send a message to all clients
-const broadcast = (message, sender)=>{
+const broadcast = (message, sender) => {
   clients.forEach((client) => {
-    // Don't want to send it to sender
     if (client === sender) return;
     client.write(message);
   });
-  // Log it to the server output too
   log.info(message);
-}
+};
 
-const server = net.createServer((socket) =>{
+const server = net.createServer((socket) => {
   socket.name = `${socket.remoteAddress}:${socket.remotePort}`;
   clients.push(socket);
 
   socket.write(`Welcome ${socket.name}\n`);
   broadcast(`${socket.name} joined the chat\n`, socket);
 
-  // Handle incoming messages from clients.
   socket.on('data', (data) => {
     broadcast(`${socket.name}> ${data}`, socket);
   });
 
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
+  socket.on('end', () => {
     clients.splice(clients.indexOf(socket), 1);
     broadcast(`${socket.name} left the chat.\n`);
   });
-
-
 });
 
 server.listen(5000);
